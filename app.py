664d23ba95a0b0f0031d4489f04011803b8c35e1
Mhmd_dev <mhmdaleshawi@gmail.com>
2024-05-23 03:46:11 +0300

    adding pleacholders for the login-reset password forms , adding tearms and conditions and private poloicy

--- app.py
+++ app.py
@@ -707,6 +707,13 @@ def contact_us():
 def about_us():
     return render_template('about_us.html')
 
+@app.route('/terms_and_conditions')
+def terms_and_conditions():
+    return render_template('terms_and_conditions.html')
+
+@app.route('/privacy_policy')
+def privacy_policy():
+    return render_template('privacy_policy.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
